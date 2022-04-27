decc79ad8aa053789b9a274d0d0eb02d0c769ab3
Roman <romanshe@gmail.com>
2022-04-27 12:55:48 +0600

    first commit empty project

---
+++ src/main/java/vip/almaty/costtrackingapp/CostTrackingAppApplication.java
@@ -0,0 +1,13 @@
+package vip.almaty.costtrackingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostTrackingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostTrackingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vip/almaty/costtrackingapp/CostTrackingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.almaty.costtrackingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostTrackingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
