837808fe0cdbd27da63bb637e373e800622e1fb2
Roman <romanshe@gmail.com>
2022-05-12 11:13:09 +0600

    Implements spent and remaining amounts in the budget view and calculations in the Category class

--- src/main/java/vip/almaty/costtrackingapp/domain/Category.java
+++ src/main/java/vip/almaty/costtrackingapp/domain/Category.java
@@ -51,7 +51,6 @@ public class Category
     {
         this.group = group;
     }
-
     @OneToMany(cascade=CascadeType.PERSIST, fetch=FetchType.LAZY, mappedBy="category")
     @OrderBy("date DESC")
     public Set<Transaction> getTransactions()
@@ -61,5 +60,46 @@ public class Category
     public void setTransactions(Set<Transaction> transactions)
     {
         this.transactions = transactions;
+    }
+
+    @Transient
+    public BigDecimal getSpent() {
+        double sum = this.getTransactions().stream()
+                .mapToDouble(t -> {
+                    if (t.getTotal() == null)
+                        return 0.0;
+                    else
+                        return t.getTotal().doubleValue();
+                })
+                .sum();
+        return BigDecimal.valueOf(sum);
+    }
+    @Transient
+    public BigDecimal getRemaining() {
+        return this.getBudget().subtract(this.getSpent());
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Category other = (Category) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }
